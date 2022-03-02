86a6d501895abe4476d814d94a85c43424d458b8
Amélie <amelie.ledein@orange.fr>
2022-03-02 14:17:47 +0100

    Add .mli

---
+++ src/interface/output.mli
@@ -0,0 +1,4 @@
+val readable : bool ref
+
+(** To make readable symbol name *)
+val pp : string -> string
---
+++ src/printing/Kore_printer.mli
@@ -0,0 +1,11 @@
+
+(*  *type output = Format.formatter *)
+
+open Mecanism.Count_data
+open Common.Type
+
+val verbose : bool ref
+
+val pp_kore_hooked_sort : Format.formatter -> sort -> attribute list -> unit
+val pp_kore_import  : Format.formatter -> import -> unit
+val pp_kore_kommand : Format.formatter -> count_data -> kommand list -> unit
