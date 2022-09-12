9162f61f8b4c374941782cf9cbb13ccd6dea04b5
Amélie <amelie.ledein@orange.fr>
2022-09-12 10:05:13 +0200

    Fix bug when there is no conditional rewriting rule

--- src/printing/meta_printer.ml
+++ src/printing/meta_printer.ml
@@ -14,10 +14,10 @@ let prt_Viry ppc cd prt :
   fun (sort_l, sym_l, sym_add_l, r_l) ->
   List.iter (fun x -> incr_real_symbol cd ; prt ppc x) (List.rev sort_l) ;
   List.iter (fun x -> incr_real_symbol cd ; prt ppc x) (List.rev sym_l)  ;
-  if List.length sym_add_l > 3 then
+  if List.length sym_add_l > 3 then (* If there is at least one conditional rewriting rule *)
     (List.iter
        (fun x -> incr_additional_symbol cd ; prt ppc (create_LP_symbol x))
-       (List.rev sym_add_l) ;
-     List.iter
-       (fun x -> incr_real_rule cd ; prt ppc (create_multi_rule [x]))
-       (List.rev r_l))
+       (List.rev sym_add_l)) ;
+  List.iter
+    (fun x -> incr_real_rule cd ; prt ppc (create_multi_rule [x]))
+    (List.rev r_l)
