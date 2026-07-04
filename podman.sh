a59f40467b5484f09c2adcc60e1510f29a76f237
Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
2026-07-04 13:43:40 -0600

    chore: fix typo

--- podman.sh
+++ podman.sh
@@ -1,16 +1,16 @@
 set -e
 
 IMAGE_NAME="kamelo"
-CONTAINER_NAME="test-kamelo"
+CONTAINER_NAME="my-kamelo"
 ROOT="/kamelo"
 CONTAINER_LP_DIR="$ROOT/lp-generated"
 HOST_LP_DIR="$(pwd)/lp-generated"
 
-if podman image exists "%IMAGE_NAME"; then
+if podman image exists "$IMAGE_NAME"; then
   echo "Image exists. Skipping build."
 else
   echo "Building image..."
-  podman build -t "$IMAGE_NAME"
+  podman build -t "$IMAGE_NAME" .
 fi
 
 echo "Running container..."
